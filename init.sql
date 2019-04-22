2fa8f9f09cdb33d8255e02f80a10fc95cc2cb783
mz-git <49807892+mz-git@users.noreply.github.com>
2019-04-22 12:37:47 +0800

    Update init.sql

--- init.sql
+++ init.sql
@@ -11,3 +11,19 @@ CREATE TABLE `student` (
   `createTime` datetime DEFAULT NULL COMMENT '建表日期',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `score`;
+CREATE TABLE `score` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `st_id` bigint(20) DEFAULT NULL,
+  `co_id` bigint(20) DEFAULT NULL,
+  `score` double(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `course`;
+CREATE TABLE `course` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `course` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
